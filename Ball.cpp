1fba91ab1105d194de911f7614797dc57d4dbf69
tomneoball <tomsonnenberg00@gmail.com>
2025-12-16 14:15:49 +0100

    Modify Ball::update to handle topLimit for collisions
    Updated the Ball::update method to include a topLimit parameter for collision detection.

--- Ball.cpp
+++ Ball.cpp
@@ -8,13 +8,29 @@ Ball::Ball(float x, float y, float vx, float vy, float scale)
     rect = { x, y, size, size };
 }
 
-void Ball::update(float dt, int winW, int winH) {
+void Ball::update(float dt, int winW, int winH, float topLimit) {
     rect.x += velX * dt;
     rect.y += velY * dt;
 
-    if (rect.x < 0) { rect.x = 0; velX *= -1; }
-    if (rect.x > (float)winW - rect.w) { rect.x = (float)winW - rect.w; velX *= -1; }
-    if (rect.y < 0) { rect.y = 0; velY *= -1; }
+    // Links abprallen
+    if (rect.x < 0) {
+        rect.x = 0;
+        velX *= -1;
+    }
+    // Rechts abprallen
+    if (rect.x > (float)winW - rect.w) {
+        rect.x = (float)winW - rect.w;
+        velX *= -1;
+    }
 
-    if (rect.y > (float)winH) active = false;
+    // Oben abprallen (am neuen topLimit der Arena!)
+    if (rect.y < topLimit) {
+        rect.y = topLimit;
+        velY *= -1;
+    }
+
+    // Unten rausfliegen (Game Over Bedingung für diesen Ball)
+    if (rect.y > (float)winH) {
+        active = false;
+    }
 }
